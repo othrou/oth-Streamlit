4c2baa98ab0372dd900bae355dfd5195e6ef701e
othrou <157309232+othrou@users.noreply.github.com>
2024-08-17 10:40:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 st.title('🎈 Machine Learning App')
@@ -113,5 +114,6 @@ st.dataframe(df_prediction_proba,
 
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
+
 
 
